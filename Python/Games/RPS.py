807f17d37aab9a6a5f0171efd94851c9e3a02743
161 of clubs <cuendem05@gmail.com>
2025-10-06 19:42:00 +0200

    Slight logic tweak

--- Python/Games/RPS.py
+++ Python/Games/RPS.py
@@ -28,46 +28,24 @@ def playerChoice():
 
 def result(p, e):
     global playerWins, enemyWins, ties
+    wins_against = {
+        "Pedra": ["Tissores", "Llangardaix"],
+        "Paper": ["Pedra", "Spock"],
+        "Tissores": ["Paper", "Llangardaix"],
+        "Llangardaix": ["Spock", "Paper"],
+        "Spock": ["Tissores", "Pedra"]
+    }
+
     if p == e:
         ties += 1
         print("Empat!")
+    elif e in wins_against[p]:
+        playerWins += 1
+        print("Tu guanyes!")
     else:
-        if p == "Pedra":
-            if e in ["Tissores", "Llangardaix"]:
-                playerWins += 1
-                print("Tu guanyes!")
-            else:
-                enemyWins += 1
-                print("L'oponent guanya!")
-        elif p == "Paper":
-            if e in ["Pedra", "Spock"]:
-                playerWins += 1
-                print("Tu guanyes!")
-            else:
-                enemyWins += 1
-                print("L'oponent guanya!")
-        elif p == "Tissores":
-            if e in ["Paper", "Llangardaix"]:
-                playerWins += 1
-                print("Tu guanyes!")
-            else:
-                enemyWins += 1
-                print("L'oponent guanya!")
-        elif p == "Llangardaix":
-            if e in ["Spock", "Paper"]:
-                playerWins += 1
-                print("Tu guanyes!")
-            else:
-                enemyWins += 1
-                print("L'oponent guanya!")
-        elif p == "Spock":
-            if e in ["Tissores", "Pedra"]:
-                playerWins += 1
-                print("Tu guanyes!")
-            else:
-                enemyWins += 1
-                print("L'oponent guanya!")
-    
+        enemyWins += 1
+        print("L'oponent guanya!")
+
     print(f"Tu: {playerWins}\nOponent: {enemyWins}")
 
 def main():
